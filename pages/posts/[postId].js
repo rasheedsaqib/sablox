63f4f4c41062ce96684683f00c670d61f44e8742
unknown <=>
2022-02-22 00:18:27 +0500

    Test deploy

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -22,8 +22,10 @@ const Post = props => {
 
 export async function getStaticProps() {
 
-    const posts = await axios.get('/posts');
+    const posts = await axios.get('/posts') || [];
     const constants = await axios.get('/constants');
+
+    console.log(posts.data);
 
     return {
         props: {
